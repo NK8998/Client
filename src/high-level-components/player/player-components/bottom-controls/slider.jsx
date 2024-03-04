6e6318a3d2a9abe7bbfec0505850575460f7e8d1
NK8998 <ne.kioko16@gmail.com>
2024-03-04 23:24:00 +0300

    asjnd

--- src/high-level-components/player/player-components/bottom-controls/slider.jsx
+++ src/high-level-components/player/player-components/bottom-controls/slider.jsx
@@ -7,9 +7,7 @@ export default function VolumeSlider({ handleFocus }) {
   const [handleMouseMove] = usePlayerMouseMove();
   const [volume, setVolume] = useState(1);
   const currentVolume = useRef(1);
-  const sliderThumbLeft = useRef(`calc(100% - 6px)`);
-  const sliderThumbRef = useRef();
-  const sliderTrackRef = useRef();
+
   const hoveringRef = useRef();
 
   const hanldeValueChange = (value) => {
@@ -20,9 +18,6 @@ export default function VolumeSlider({ handleFocus }) {
     setVolume(volume);
     currentVolume.current = volume;
     videoRef.volume = volume;
-    const parent = document.querySelector(".SliderThumb").parentNode;
-    const left = parent.style.left;
-    sliderThumbLeft.current = left;
   };
 
   let volumeIcon = <></>;
@@ -51,19 +46,14 @@ export default function VolumeSlider({ handleFocus }) {
 
   const handleMuteUnmute = () => {
     const videoRef = document.querySelector("#html5-player");
-    const sliderThumb = document.querySelector(".SliderThumb");
-    const parent = sliderThumb.parentNode;
-    const sliderWidth = sliderThumbRef.current.clientWidth / 2;
 
     if (!videoRef) return;
     if (volume > 0) {
       setVolume(0);
       videoRef.volume = 0;
-      parent.style.left = `calc(0% + ${sliderWidth}px)`;
     } else if (volume === 0) {
       setVolume(currentVolume.current);
       videoRef.volume = currentVolume.current;
-      parent.style.left = `${sliderThumbLeft.current}`;
     }
   };
 
@@ -81,10 +71,10 @@ export default function VolumeSlider({ handleFocus }) {
       </button>
       <form className='volume-slider player-button' tabIndex={0} onFocus={handleMouseEnter} onBlur={handleBlur}>
         <Slider.Root className='SliderRoot' value={[volume * 100]} defaultValue={[100]} max={100} min={0} step={1} onValueChange={hanldeValueChange}>
-          <Slider.Track className='SliderTrack' ref={sliderTrackRef}>
+          <Slider.Track className='SliderTrack'>
             <Slider.Range className='SliderRange' />
           </Slider.Track>
-          <Slider.Thumb className='SliderThumb' aria-label='Volume' ref={sliderThumbRef} />
+          <Slider.Thumb className='SliderThumb' aria-label='Volume' />
         </Slider.Root>
       </form>
     </>
