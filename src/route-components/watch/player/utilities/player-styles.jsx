37067d8dfdc730eab26158ede5eaa1d9d3cb61c2
NK8998 <ne.kioko16@gmail.com>
2024-03-20 17:11:06 +0300

    asd

--- src/route-components/watch/player/utilities/player-styles.jsx
+++ src/route-components/watch/player/utilities/player-styles.jsx
@@ -32,7 +32,6 @@ export const usePlayerStyles = () => {
   }
 
   const calculateWidth = () => {
-    // console.log("running");
     const root = document.querySelector("#root");
     const secondaryRef = document.querySelector(".secondary.content");
     if (!root) return;
